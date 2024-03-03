e22613eb35d362149c5f0e7c69e338ab1e6c8f6c
stanyyx <125898976+stanyyx@users.noreply.github.com>
2024-03-03 20:57:46 +0300

    Create views.py

---
+++ logistic/views.py
@@ -0,0 +1,24 @@
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends=[DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = LimitOffsetPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends=[DjangoFilterBackend, SearchFilter]
+    search_fields = ['products__title', 'products__description']
+    filterset_fields = ['products',]
+    pagination_class = LimitOffsetPagination
